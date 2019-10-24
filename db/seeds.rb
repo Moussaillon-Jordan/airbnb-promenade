607f90562037dfb2841fbbd8cee88fbe34f73688
Jordan <jordan.pedro27@gmail.com>
2019-10-24 19:28:09 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+4.times do
+  city = City.create!(city_name: Faker::Address.city)
+end
+
+4.times do
+  dogsitter = Dogsitter.create!(name: Faker::Name.name, city: City.all.sample)
+end
+
+8.times do
+  dog = Dog.create!(name: Faker::Name.first_name, city: City.all.sample)
+end
+
+12.times do
+  stroll = Stroll.create!(date: Faker::Date.between(from: 2.years.ago, to: Date.today), dogsitter: Dogsitter.all.sample, dog: Dog.all.sample)
+end
